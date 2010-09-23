ad1b91e5741dcd46549df98911f57d7b6129e01e
Aditya Mukerjee <aditya@Zazou.(none)>
2010-09-23 19:19:28 -0400

    Renamed ballot variable names to be more comprehensible

--- BallotParser.py
+++ BallotParser.py
@@ -28,49 +28,52 @@ class Ballot:
 class BallotClassTester(unittest.TestCase):
     '''Test the Ballot class'''
     def setUp(self):
-        self.abc_candidate_list = ['Adam', 'Bob', 'Charles']
-        self.abc_ballot = Ballot(self.abc_candidate_list)
-        self.defghijk_candidates_list = ['Donald', 'Edward', 'Francis', 'Greta', 'Hilda', 'Igor', 'John', 'Kathy']
-        self.defghijk_ballot= Ballot(self.defghijk_candidates_list)
-        self.abcdefghijk_candidate_list = list(self.abc_candidate_list)
-        self.abcdefghijk_candidate_list.extend(self.defghijk_candidates_list)
-        self.abcdefghijk_ballot = Ballot(self.abcdefghijk_candidate_list)
+        self.a_to_c_candidate_list = ['Adam', 'Bob', 'Charles']
+        self.a_to_c_ballot = Ballot(self.a_to_c_candidate_list)
+        self.d_to_k_candidates_list = ['Donald', 'Edward', 'Francis', 'Greta', 'Hilda', 'Igor', 'John', 'Kathy']
+        self.d_to_k_ballot= Ballot(self.d_to_k_candidates_list)
+        self.a_to_k_candidate_list = list(self.a_to_c_candidate_list)
+        self.a_to_k_candidate_list.extend(self.d_to_k_candidates_list)
+        self.a_to_k_ballot = Ballot(self.a_to_k_candidate_list)
 
     def test_create_sample_three_way_ballot(self):
         '''Create a three-way ballot and check that the candidates are correct'''
-        ballot = Ballot(self.abc_candidate_list)
-        self.assertEqual(ballot.candidates, self.abc_candidate_list)
+        ballot = Ballot(self.a_to_c_candidate_list)
+        self.assertEqual(ballot.candidates, self.a_to_c_candidate_list)
 
     def test_candidate_name_generator(self):
         '''Test that the ballot can eliminate candidates and return the next eligible candidate accurately'''
-        for rank, candidate in enumerate(self.defghijk_ballot.next_candidate()):
-            self.assertEqual(self.defghijk_candidates_list[rank], candidate)
+        #Use next_candidate() to generate the next eligible candidate
+        for rank, candidate in enumerate(self.d_to_k_ballot.next_candidate()):
+            print(rank, candidate)
+            self.assertEqual(self.d_to_k_candidates_list[rank], candidate)
             #Eliminate the current candidate so that he/she is no longer the top eligible candidate
-            self.defghijk_ballot.eliminated_candidates.add(candidate)
+            self.d_to_k_ballot.eliminated_candidates.add(candidate)
 
     def test_candidate_name_generator_with_elimination(self):
         '''Test that the ballot can yield the same candidate twice, then yield a different candidate once the first candidate is eliminated'''
         count = 0
-        for rank, candidate in enumerate(self.defghijk_ballot.next_candidate()):
+        for rank, candidate in enumerate(self.d_to_k_ballot.next_candidate()):
             #Check that the candidate that .next_candidate() yields is not in the list of eliminated candidates
-            self.assertTrue(candidate not in self.defghijk_ballot.eliminated_candidates)
+            print(candidate)
+            self.assertTrue(candidate not in self.d_to_k_ballot.eliminated_candidates)
             count += 1
             if count > 2:
                 count = 0
-                self.defghijk_ballot.eliminated_candidates.add(candidate)
+                self.d_to_k_ballot.eliminated_candidates.add(candidate)
 
     def test_eliminate_candidate_function(self):
         '''Test Ballot.eliminate_candidate()'''
-        first_candidate_ballot_1 = self.abc_ballot.next_candidate()
+        first_candidate_ballot_1 = self.a_to_c_ballot.next_candidate()
         Ballot.eliminate_candidate(first_candidate_ballot_1)
-        second_candidate_ballot_1 = self.abc_ballot.next_candidate()
-        second_candidate_ballot_2 = self.abcdefghijk_ballot.next_candidate
+        second_candidate_ballot_1 = self.a_to_c_ballot.next_candidate()
+        second_candidate_ballot_2 = self.a_to_k_ballot.next_candidate
         self.assertTrue(first_candidate_ballot_1 != second_candidate_ballot_2)
         self.assertTrue(second_candidate_ballot_1, second_candidate_ballot_2)
 
     def test_setup_feature(self):
         '''Test that the setUp() method is functioning properly'''
-        self.assertEqual(self.abc_ballot.candidates, ['Adam', 'Bob', 'Charles'])
+        self.assertEqual(self.a_to_c_ballot.candidates, ['Adam', 'Bob', 'Charles'])
 
 
 class BallotBox:
