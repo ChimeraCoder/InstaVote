d43dae3695bcf0c299184544269d26ca0076a9eb
Aditya Mukerjee <aditya@Zazou.(none)>
2010-09-24 13:25:25 -0400

    BallotParser now identifies a winner for the election

--- BallotParser.py
+++ BallotParser.py
@@ -19,6 +19,7 @@ class Ballot:
                 yield self.candidates[candidate_index]
 
     eliminated_candidates = set()
+
     @classmethod
     def eliminate_candidate(cls, newly_eliminated_candidate):
         '''Update the set of eliminated candidates with the newly eliminated candidate'''
@@ -28,6 +29,8 @@ class Ballot:
 class BallotClassTester(unittest.TestCase):
     '''Test the Ballot class'''
     def setUp(self):
+        #Make sure the set of eliminated_candidates is clear at the beginning of each unit test
+        Ballot.eliminated_candidates = set()
         self.a_to_c_candidate_list = ['Adam', 'Bob', 'Charles']
         self.a_to_c_ballot = Ballot(self.a_to_c_candidate_list)
         self.d_to_k_candidates_list = ['Donald', 'Edward', 'Francis', 'Greta', 'Hilda', 'Igor', 'John', 'Kathy']
@@ -43,24 +46,30 @@ class BallotClassTester(unittest.TestCase):
 
     def test_candidate_name_generator(self):
         '''Test that the ballot can eliminate candidates and return the next eligible candidate accurately'''
+        entered_loop = False
         #Use next_candidate() to generate the next eligible candidate
         for rank, candidate in enumerate(self.d_to_k_ballot.next_candidate()):
-            print(rank, candidate)
+            entered_loop = True
             self.assertEqual(self.d_to_k_candidates_list[rank], candidate)
             #Eliminate the current candidate so that he/she is no longer the top eligible candidate
             self.d_to_k_ballot.eliminated_candidates.add(candidate)
+        #Ensure that the loop was actually executed
+        self.assertTrue(entered_loop)
 
     def test_candidate_name_generator_with_elimination(self):
         '''Test that the ballot can yield the same candidate twice, then yield a different candidate once the first candidate is eliminated'''
         count = 0
+        entered_loop = False
         for rank, candidate in enumerate(self.d_to_k_ballot.next_candidate()):
+            entered_loop = True
             #Check that the candidate that .next_candidate() yields is not in the list of eliminated candidates
-            print(candidate)
-            self.assertTrue(candidate not in self.d_to_k_ballot.eliminated_candidates)
             count += 1
-            if count > 2:
+            self.assertTrue(candidate not in self.d_to_k_ballot.eliminated_candidates)
+            if count > 1:
                 count = 0
                 self.d_to_k_ballot.eliminated_candidates.add(candidate)
+        #Ensure that the loop was actually executed
+        self.assertTrue(entered_loop)
 
     def test_eliminate_candidate_function(self):
         '''Test Ballot.eliminate_candidate()'''
@@ -104,11 +113,33 @@ class BallotBox:
     def count_votes(self):
         vote_counts = dict()
         for candidate in self.candidate_names:
+            if candidate in Ballot.eliminated_candidates:
+                continue
             vote_counts[candidate] = 0
         for ballot in self.ballots:
-            print(ballot.next_candidate())
-            vote_counts[ballot.next_candidate()] += 1
-        return 
+            candidate = next(ballot.next_candidate())
+            vote_counts[candidate]+= 1
+        return vote_counts 
+
+    def find_minimum(self, vote_counts):
+        return min(vote_counts, key = lambda x: vote_counts.get(x))
+
+    def find_maximum(self, vote_counts):
+        return max(vote_counts, key = lambda x: vote_counts.get(x))
+
+    def identify_winner(self):
+        winning_number = len(self.ballots)//2 + 1
+        print(winning_number)
+        winner_found = False
+        while not winner_found:
+           vote_counts = self.count_votes()
+           best_candidate = self.find_maximum(vote_counts)
+           if vote_counts[best_candidate] >= winning_number:
+               return best_candidate
+           else:
+               worst_candidate = self.find_minimum(vote_counts)
+               Ballot.eliminate_candidate(worst_candidate)
+               print(worst_candidate) 
 
 
 class BallotBoxTester(unittest.TestCase):
@@ -124,6 +155,8 @@ class BallotBoxTester(unittest.TestCase):
             ]
         self.test_ballot_file = 'testfiles/test_ballot.csv'
         self.ballot_box = BallotBox(self.test_ballot_file)
+        #Make sure the set of eliminated_candidates is clear at the beginning of each unit test
+        Ballot.eliminated_candidates = set()
 
     def test_ballot_generator(self):
         '''Test that a ballot is being constructed correctly from a .csv file'''
@@ -143,17 +176,34 @@ class BallotBoxTester(unittest.TestCase):
         for ballot in self.ballot_box.ballots:
             self.assertTrue('Aditya' in ballot.eliminated_candidates)
 
-
     def test_count_votes_in_first_round(self):
+        '''Test that the BallotBox can count the number of votes in the first round'''
         actual_counts = {
             'Kaley': 1,
             'Roxanne': 3,
             'Aditya' : 2,
+            'Michael' : 0,
                 }
         vote_results = self.ballot_box.count_votes()
         for key in vote_results:
             self.assertTrue(vote_results[key] == actual_counts[key])
 
+    def test_find_minimum_votes_in_round(self):
+        '''Test that the BallotBox can correctly identify the first candidate to be eliminated'''
+        first_round_vote_resuts = self.ballot_box.count_votes()
+        round_loser = self.ballot_box.find_minimum(first_round_vote_resuts)
+        self.assertTrue(round_loser == 'Michael')
+
+    def test_find_minimum_votes_in_round(self):
+        '''Test that the BallotBox can correctly identify the candidate with the most votes in a round'''
+        first_round_vote_resuts = self.ballot_box.count_votes()
+        round_loser = self.ballot_box.find_maximum(first_round_vote_resuts)
+        self.assertTrue(round_loser == 'Roxanne')
+
+
+    def test_identify_winner(self):
+        winner = self.ballot_box.identify_winner()
+        self.assertTrue(winner == 'Roxanne')
 
 
 if __name__ == '__main__':
