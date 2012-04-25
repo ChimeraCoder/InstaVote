e3c8bf6b93aa610543edabaee85eb2761ef44207
aditya <dev@chimeracoder.net>
2012-04-25 19:30:47 -0400

    Created setup.py file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='InstaVote',
+    version='0.1.0',
+    author='Aditya Mukerjee',
+    author_email='dev@chimeracoder.net',
+    packages=['instavote',],
+    url='https://github.com/ChimeraCoder/InstaVote',
+    license='LICENSE',
+    description='A Python module that determines the winner of an election using instant-runoff voting rules',
+    long_description=open('README.txt').read(),
+    install_requires=[],
+)
