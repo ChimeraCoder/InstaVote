ee389c19fbcc2ea655dff28910d6a7553ae05434
aditya <dev@chimeracoder.net>
2012-04-25 19:05:34 -0400

    Moved tests from main module into separate .py files
    Moved tests out of BallotParser.py
    
    Removed naked ./testfiles/ directory

---
+++ tests/BallotBoxTester.py
@@ -0,0 +1,73 @@
+from __future__ import division, print_function
+import unittest
+from BallotParser import Ballot, BallotBox
+
+class BallotBoxTester(unittest.TestCase):
+    '''Test the BallotBox class'''
+    def setUp(self):
+        self.test_ballot_1_known_values = [
+            ('Kaley', 'Roxanne', 'Aditya'),
+            ('Roxanne', 'Kaley', 'Aditya'),
+            ('Aditya', 'Kaley', 'Roxanne'),
+            ('Aditya', 'Roxanne', 'Kaley'),
+            ('Roxanne', 'Aditya', 'Kaley'),
+            ('Roxanne', 'Aditya', 'Michael'),
+            ]
+        self.test_ballot_1_file = 'tests/testfiles/test_ballot.csv'
+        self.ballot_box_1 = BallotBox(self.test_ballot_1_file)
+        #Make sure the set of eliminated_candidates is clear at the beginning of each unit test
+        Ballot.eliminated_candidates = set()
+
+    def test_ballot_generator(self):
+        '''Test that a ballot is being constructed correctly from a .csv file'''
+        for ballot_number, ballot in enumerate(self.ballot_box_1.ballots):
+            for candidate_number, candidate in enumerate(ballot.candidates):
+                expected_candidate = self.test_ballot_1_known_values[ballot_number][candidate_number]
+                self.assertEqual(candidate, expected_candidate)
+
+    def test_name_all_candidates(self):
+        '''Test that the candidates' names are being aggregated correctly in the master set of eligible candidates'''
+        expected = set(('Kaley', 'Roxanne', 'Aditya', 'Michael'))
+        self.assertEqual(self.ballot_box_1.candidate_names, expected)
+
+    def test_eliminate_candidates_from_ballot_box(self):
+        '''Test that the ballot box is able to eliminate candidates properly'''
+        Ballot.eliminate_candidate('Aditya')
+        for ballot in self.ballot_box_1.ballots:
+            self.assertTrue('Aditya' in ballot.eliminated_candidates)
+
+    def test_count_votes_in_first_round(self):
+        '''Test that the BallotBox can count the number of votes in the first round'''
+        actual_counts = {
+            'Kaley': 1,
+            'Roxanne': 3,
+            'Aditya' : 2,
+            'Michael' : 0,
+                }
+        vote_results = self.ballot_box_1.count_votes()
+        for key in vote_results:
+            self.assertTrue(vote_results[key] == actual_counts[key])
+
+    def test_find_minimum_votes_in_round(self):
+        '''Test that the BallotBox can correctly identify the first candidate to be eliminated'''
+        first_round_vote_resuts = self.ballot_box_1.count_votes()
+        round_loser = self.ballot_box_1.find_minimum(first_round_vote_resuts)
+        self.assertTrue(round_loser == 'Michael')
+
+    def test_find_minimum_votes_in_round(self):
+        '''Test that the BallotBox can correctly identify the candidate with the most votes in a round'''
+        first_round_vote_resuts = self.ballot_box_1.count_votes()
+        round_loser = self.ballot_box_1.find_maximum(first_round_vote_resuts)
+        self.assertTrue(round_loser == 'Roxanne')
+
+
+    def test_identify_winner(self):
+        winner = self.ballot_box_1.identify_winner()
+        self.assertTrue(winner == 'Roxanne')
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
---
+++ tests/BallotClassTester.py
@@ -0,0 +1,63 @@
+from __future__ import division, print_function
+import unittest
+from BallotParser import BallotBox, Ballot
+
+class BallotClassTester(unittest.TestCase):
+    '''Test the Ballot class'''
+    def setUp(self):
+        #Make sure the set of eliminated_candidates is clear at the beginning of each unit test
+        Ballot.eliminated_candidates = set()
+        self.a_to_c_candidate_list = ['Adam', 'Bob', 'Charles']
+        self.a_to_c_ballot = Ballot(self.a_to_c_candidate_list)
+        self.d_to_k_candidates_list = ['Donald', 'Edward', 'Francis', 'Greta', 'Hilda', 'Igor', 'John', 'Kathy']
+        self.d_to_k_ballot= Ballot(self.d_to_k_candidates_list)
+        self.a_to_k_candidate_list = list(self.a_to_c_candidate_list)
+        self.a_to_k_candidate_list.extend(self.d_to_k_candidates_list)
+        self.a_to_k_ballot = Ballot(self.a_to_k_candidate_list)
+
+    def test_create_sample_three_way_ballot(self):
+        '''Create a three-way ballot and check that the candidates are correct'''
+        ballot = Ballot(self.a_to_c_candidate_list)
+        self.assertEqual(ballot.candidates, self.a_to_c_candidate_list)
+
+    def test_candidate_name_generator(self):
+        '''Test that the ballot can eliminate candidates and return the next eligible candidate accurately'''
+        entered_loop = False
+        #Use next_candidate() to generate the next eligible candidate
+        for rank, candidate in enumerate(self.d_to_k_ballot.next_candidate()):
+            entered_loop = True
+            self.assertEqual(self.d_to_k_candidates_list[rank], candidate)
+            #Eliminate the current candidate so that he/she is no longer the top eligible candidate
+            self.d_to_k_ballot.eliminated_candidates.add(candidate)
+        #Ensure that the loop was actually executed
+        self.assertTrue(entered_loop)
+
+    def test_candidate_name_generator_with_elimination(self):
+        '''Test that the ballot can yield the same candidate twice, then yield a different candidate once the first candidate is eliminated'''
+        count = 0
+        entered_loop = False
+        for rank, candidate in enumerate(self.d_to_k_ballot.next_candidate()):
+            entered_loop = True
+            #Check that the candidate that .next_candidate() yields is not in the list of eliminated candidates
+            count += 1
+            self.assertTrue(candidate not in self.d_to_k_ballot.eliminated_candidates)
+            if count > 1:
+                count = 0
+                self.d_to_k_ballot.eliminated_candidates.add(candidate)
+        #Ensure that the loop was actually executed
+        self.assertTrue(entered_loop)
+
+    def test_eliminate_candidate_function(self):
+        '''Test Ballot.eliminate_candidate()'''
+        first_candidate_ballot_1 = self.a_to_c_ballot.next_candidate()
+        Ballot.eliminate_candidate(first_candidate_ballot_1)
+        second_candidate_ballot_1 = self.a_to_c_ballot.next_candidate()
+        second_candidate_ballot_2 = self.a_to_k_ballot.next_candidate
+        self.assertTrue(first_candidate_ballot_1 != second_candidate_ballot_2)
+        self.assertTrue(second_candidate_ballot_1, second_candidate_ballot_2)
+
+    def test_setup_feature(self):
+        '''Test that the setUp() method is functioning properly'''
+        self.assertEqual(self.a_to_c_ballot.candidates, ['Adam', 'Bob', 'Charles'])
+
+
