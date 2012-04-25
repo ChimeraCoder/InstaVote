0dc36cc26e8f86a694cb33c0972c310ca9d2a85e
aditya <dev@chimeracoder.net>
2012-04-25 19:48:26 -0400

    Main action moved to main() function to allow for file imports

--- instavote/instant_runoff_calculator.py
+++ instavote/instant_runoff_calculator.py
@@ -2,22 +2,26 @@ from __future__ import division, print_function
 import BallotParser
 import sys
 
-ballot_created = False
+def main(infile):
+    ballot_created = False
 
-while not ballot_created:
-    try:
-        ballots_file = sys.argv[1]
-        ballot_box = BallotParser.BallotBox(ballots_file)
-        ballot_created = True
-    except (IndexError, IOError):
-        print('Please enter the relative path to the file')
-        sys.argv = [None, raw_input(),]
+    while not ballot_created:
+        try:
+            ballots_file = infile
+            ballot_box = BallotParser.BallotBox(ballots_file)
+            ballot_created = True
+        except (IndexError, IOError):
+            print('Please enter the relative path to the file')
+            sys.argv = [None, raw_input(),]
 
 
-for ballot in ballot_box.ballots:
-    print(ballot.candidates)
- 
- 
+    for ballot in ballot_box.ballots:
+        print(ballot.candidates)
+     
+     
 
-print(ballot_box.identify_winner(), 'is the winner')
-print(ballot_box.number_rounds, 'rounds of voting were run')
+    print(ballot_box.identify_winner(), 'is the winner')
+    print(ballot_box.number_rounds, 'rounds of voting were run')
+
+if __name__ == '__main__':
+    main(sys.argv[1])
